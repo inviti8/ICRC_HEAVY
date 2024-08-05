4bfa40c9f4439a26d73caf4008c30ca2e756b892
Jason <dimensions3@outlook.com>
2024-08-05 13:22:07 -0700

    fix

--- runners/test_lifecycle.sh
+++ runners/test_lifecycle.sh
@@ -12,7 +12,7 @@ for i in `seq 2 $max`
 do
     dfx identity new $user$i --storage-mode=plaintext || true
     dfx identity use $user$i
-    account_id=$(icx principal-convert --to-hex $(dfx identity get-principal) 2>&1  | cut -d ' ' -f 2); echo $(printf '%x\n' $(echo $account_id | wc -c | xargs -I{} expr {} / 2))$(echo $X | xargs printf "%-62s\n" | tr ' ' 0)
+    account_id=$(icx principal-convert --to-hex $(icx principal-convert --to-hex $(dfx identity get-principal) 2>&1  | cut -d ' ' -f 2); echo $(printf '%x\n' $(echo $account_id | wc -c | xargs -I{} expr {} / 2))$(echo $account_id | xargs printf "%-62s\n" | tr ' ' 0)
     echo $account_id 
     dfx identity use ident-1
     echo "dfx ledger transfer ${account_id} --amount 1000 --memo ${i}"
